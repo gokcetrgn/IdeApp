ee55318819792d5b510e87bc8f871b6a36dbf15a
Gokcenaz Torgan <gokcetorgan@hotmail.com>
2023-07-05 13:28:06 +0300

    son düzenlemeler yapıldı.

--- lib/onboardingpages/PageThree.dart
+++ lib/onboardingpages/PageThree.dart
@@ -12,29 +12,31 @@ class _PageThreeState extends State<PageThree> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green.shade100,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              height: 450,
-              child: Image.asset("images/Creative thinking-bro.png"),
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.02,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "Unutmadan! Sadece fikirlerini değil, her türden notlarını saklayabilirsin. İster okul ister iş hayatın için indir, senin için buradayız. ",
-                style: TextStyle(
-                    fontSize: MediaQuery.of(context).size.shortestSide * 0.06,
-                    color: Colors.green.shade600,
-                    fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 450,
+                child: Image.asset("images/Creative thinking-bro.png"),
               ),
-            ),
-          ],
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.02,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Unutmadan! Sadece fikirlerini değil, her türden notlarını saklayabilirsin. İster okul ister iş hayatın için indir, senin için buradayız. ",
+                  style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.shortestSide * 0.06,
+                      color: Colors.green.shade600,
+                      fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/onboardingpages/PageTwo.dart
+++ lib/onboardingpages/PageTwo.dart
@@ -12,29 +12,31 @@ class _PageTwoState extends State<PageTwo> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green.shade100,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              height: 450,
-              child: Image.asset("images/Creativity-amico.png"),
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.01,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "IdeApp olarak fikirlerinin değerinin farkındayız. Onları kategorileştirip senin için tutuyoruz! Ayrıca uygulama içinde ilham da alabilirsin :)",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: Colors.green.shade500,
-                    fontWeight: FontWeight.bold,
-                    fontSize: MediaQuery.of(context).size.shortestSide * 0.06),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 450,
+                child: Image.asset("images/Creativity-amico.png"),
               ),
-            ),
-          ],
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.01,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "IdeApp olarak fikirlerinin değerinin farkındayız. Onları kategorileştirip senin için tutuyoruz! Ayrıca uygulama içinde ilham da alabilirsin :)",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.green.shade500,
+                      fontWeight: FontWeight.bold,
+                      fontSize: MediaQuery.of(context).size.shortestSide * 0.06),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
